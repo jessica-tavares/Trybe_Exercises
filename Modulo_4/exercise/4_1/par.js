68d4fff9d63e9507e89c64c4ffcf20bf6a57cc29
jessica-tavares <jessicatavareseel@gmail.com>
2020-03-26 18:34:39 -0300

    Exercicios 5, 6, 7 e 8

---
+++ Modulo_4/exercise/4_1/par.js
@@ -0,0 +1,12 @@
+let num1 = 7;
+let num2= 5;
+let num3 = 9;
+let result = undefined;
+
+if (num1%2==0 || num2%2==0 || num3%2==0){
+    result = true;
+} else {
+    result = false;
+}
+
+console.log(result)
---
+++ Modulo_4/exercise/4_1/triAngulos.js
@@ -0,0 +1,12 @@
+let ang1 = 90;
+let ang2 = 40;
+let ang3 = 50;
+let eUmTriangulo = undefined;
+
+if (ang1+ang2+ang3 == 180){
+    eUmTriangulo = true;
+}
+else{
+    eUmTirangulo = false;
+    console.log("Erro: Tais Angulos, não formam um Triângulo!");
+}
