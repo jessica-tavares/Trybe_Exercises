9754d08aaff15eb36fdffecc5b3265848809b1ca
jessica-tavares <jessicatavareseel@gmail.com>
2020-03-26 19:36:33 -0300

    ultimo exercicio do Diagit add .!

---
+++ Modulo_4/exercise/4_1/lucroProduto.js
@@ -0,0 +1,12 @@
+let custo = 48;
+let valorDeVenda = 90;
+let lucroMil = undefined;
+
+
+if (custo!=0 && valorDeVenda!=0){
+  lucroMil = 1000*(valorDeVenda - custo*1.2);
+} else {
+  console.log("ERRO: um dos valores é menor de zero!");
+}
+
+console.log(lucroMil);
