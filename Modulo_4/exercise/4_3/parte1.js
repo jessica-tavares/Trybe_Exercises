606d0f99f62dc1ee35b206e15cb57a10e9aeaf60
jessica-tavares <jessicatavareseel@gmail.com>
2020-03-31 02:49:29 -0300

    Parte 1 comentada e exercicio 5 resolvido

--- Modulo_4/exercise/4_3/parte1.js
+++ Modulo_4/exercise/4_3/parte1.js
@@ -1,28 +1,39 @@
 let info = {
-  personagem: ["Margarida"], 
-  origem: ["Pato Donald"],
-  nota: ["Namorado do personagem primcipal dos quadrinhos do Pat0 Donald."],
-  recorrente: ['sim'],
+  personagem: "Margarida", 
+  origem: "Pato Donald",
+  nota: "Namorado do personagem primcipal dos quadrinhos do Pat0 Donald.",
 };
 
+//Exercício 1
 console.log("Bem vinda," + info.personagem);
 
+//Exercício 2
+info.recorrente = 'sim';
 console.log(info)
 
+//Exercício 3
 for (let i in info){
   console.log(i)
 }
 
+// Exercício 4
 for (let i in info){
   console.log(info[i])
 }
 
-
-info.personagem.push("Tio Patinhas");
-info.origem.push("Christmas on Bear Mountain, Dell’s Four Color Comics #178");
-info.nota.push("O ultimo Mc Patinhas");
-info.recorrente.push("sim");
+//Exercício 5
+var info2 = {
+  personagem : "Tio Patinhas",
+  origem : "Christmas on Bear Mountain, Dell’s Four Color Comics #178",
+  nota : "O ultimo Mc Patinhas",
+  recorrente : "sim",
+}
 
 for (let i in info){
-  console.log(info[i])
+  if (info[i] === info2[i]){
+    console.log("Ambos recorrentes");
+  }else {
+    console.log(info[i] + " e " + info2[i])
+  }
+
 }
