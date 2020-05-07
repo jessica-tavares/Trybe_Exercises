6e2c915be7ea90b406482b073f38064ed008ff5a
jessica-tavares <jessicatavareseel@gmail.com>
2020-05-07 11:00:04 -0300

    aula 8.4 com Bonus

---
+++ Modulo_8/8_4/wordLengths.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+// escreva a função wordLengths para passar nos testes abaixo:
+const wordLengths = (arr) => {
+  let result = [];
+  for (let i in arr) {
+    result[i] = arr[i].length;
+  }
+  return result
+}
+
+//
+const words = ['sun', 'potato', 'roundabout', 'pizza'];
+const expected = [3, 6, 10, 5];
+
+assert.strictEqual(typeof wordLengths, 'function');
+const output = wordLengths(words);
+assert.deepEqual(output, expected);
